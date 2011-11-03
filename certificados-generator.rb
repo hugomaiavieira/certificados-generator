1a847ef0b52ccbe70372076f6a103bc82fbeebe4
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2011-11-03 17:56:26 -0200

    Script generalizado para receber parâmetros

--- certificados-generator.rb
+++ certificados-generator.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 #
-#  Script para gerar certificados baseados em um modelo em odt.
+#  Script para gerar certificados baseados em um modelo odt (LibreOffice Writer)
+#  pegando os dados de um arquivo xls (Excel).
 #
 #  O arquivo modelo.odt deve ter uma seção¹ chamada CERIFICADOS. Dentro desta
-#  seção, devem existir as variáveis [PALESTRANTE], [PALESTRA] e [DIA], que
-#  serão substituidas pelos valores contidos no arquivo do Excel.
+#  seção, devem ser inseridas as variáveis - por exemplo [PALESTRANTE],
+#  [PALESTRA], [DIA] - que serão substituídas pelos valores contidos no arquivo
+#  dados.xls. A ordem de chamada do programa deve seguir a ordem das colunas do
+#  arquivo dados.xls.
 #
-#  O arquivo quem contém os nomes deve se chamar dados.xls. Todas as informações
-#  de cada palestrante devem estar em uma linha. As colunas devem estar
-#  exatamente na ordem: nome do palestrante, titulo da palestra, dia da palestra
+#  O nome da seção e das variáveis devem estar em letras MAIÚSCULAS, sendo que
+#  as variáveis devem estar ente [COUCHETES].
+#
+#  Exemplo de uso:
+#
+#      $ ./certificados-generator palestrante palestra dia
 #
 #  ¹ Para adicionar uma seção, vá no menu Inserir -> Seção.
 #
@@ -20,17 +26,20 @@ require "rubygems"
 require 'odf-report'
 require 'spreadsheet'
 
+PATH = File.expand_path(File.dirname(__FILE__))
+DATA = File.join(PATH, 'dados.xls')
+MODEL = File.join(PATH, 'modelo.odt')
+
 Spreadsheet.client_encoding = 'UTF-8'
 
-tabela = Spreadsheet.open('./dados.xls').worksheets[0]
+table = Spreadsheet.open(DATA).worksheets[0]
+rows = []; table.each { |row| rows << row }
 
-linhas = []; tabela.each { |linha| linhas << linha }
-
-report = ODFReport::Report.new("./modelo.odt") do |r|
-  r.add_section("CERTIFICADOS", linhas) do |s|
-    s.add_field('PALESTRANTE') { |linha| "#{linha[0]}" }
-    s.add_field('PALESTRA') { |linha| "#{linha[1]}" }
-    s.add_field('DIA') { |linha| "#{linha[2]}" }
+report = ODFReport::Report.new(MODEL) do |r|
+  r.add_section('CERTIFICADOS', rows) do |s|
+    ARGV.each_with_index do |field, i|
+      s.add_field(field.upcase) { |row| "#{row[i]}" }
+    end
   end
 end
 
